ce32e05434032adc119e827d4fd51b2317677ab2
hys <hysyeah@126.com>
2017-12-10 10:45:07 -0500

    eidt shift.c

--- chapter2/shift.c
+++ chapter2/shift.c
@@ -4,8 +4,9 @@
 int main(){
     int x = 0x63;
     int y = -0x95;
+    
+    printf("%#X\n", y);//y为负数,打印的是y的补码
     printf("%#X\n", x >> 4);
-    //printf("%#x\n", x >> 4);
     printf("%#X\n", y >> 4);
 
     return 0;
